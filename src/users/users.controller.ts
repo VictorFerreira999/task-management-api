8e4cd596ff3b30acae52ea36940509e3f4ca57b0
VictorFerreira999 <victorferreira9990@gmail.com>
2024-08-19 08:15:23 -0300

    .

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common'
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-@Controller('users')
+@Controller('users') // Define a rota base '/users'
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
